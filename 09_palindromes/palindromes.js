e8aa6e4e749b39f350b8a1a0b43cd0015ef0c749
eungureanu <64928001+eungureanu@users.noreply.github.com>
2023-02-27 21:23:24 +0200

    Add function to check if a string is palindrome; interim step is to turn lowercase and remove all non-letter characters

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (originalString) {
+    originalString=originalString.toLowerCase();
+    const regexpLettersOnly=/[a-z]/;
+    let lettersOnlyString="";
+    for (i=0; i<originalString.length; i++){
+        if (originalString[i].match(regexpLettersOnly)){
+            lettersOnlyString+=(originalString[i]);
+        }
+    }
+    let newString = "";
+    for (i=originalString.length-1; i>=0; i--){
+        if (originalString[i].match(regexpLettersOnly)){
+            newString+=(originalString[i]);
+        }
+    }
+    return (newString==lettersOnlyString);
 };
+
+palindromes ("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
