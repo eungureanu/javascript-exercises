7ad53baf17480dabd832b7a9f519e4cbf42fe466
eungureanu <64928001+eungureanu@users.noreply.github.com>
2023-02-28 23:12:02 +0200

    Add function for iterating through an array of objects using map(), to show a list with the values of a certain property from each object

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+//source for understanding how the map mathod works: https://www.freecodecamp.org/news/javascript-map-how-to-use-the-js-map-function-array-method/
+const getTheTitles = function(bookList) {
+    let titles = bookList.map(function(book) {
+        return book.title;
+        });
+    return titles;
 };
 
 // Do not edit below this line
